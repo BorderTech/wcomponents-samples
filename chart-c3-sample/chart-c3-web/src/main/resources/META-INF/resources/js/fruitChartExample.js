785b07097b1ce70220806f68032bdccc2222ce65
Jonathan Austin <jonathan.austin@border.gov.au>
2017-01-16 12:09:20 +1100

    base url change

--- chart-c3-sample/chart-c3-web/src/main/resources/META-INF/resources/js/fruitChartExample.js
+++ chart-c3-sample/chart-c3-web/src/main/resources/META-INF/resources/js/fruitChartExample.js
@@ -1,14 +1,4 @@
 //
-// Setup the C3 and D3 config.
-//
-require.config({
-    paths: {
-	d3: '/js/lib/d3.v3',
-	c3: '/js/lib/c3-0.4.11'
-    }
-});
-
-//
 // Define "FruitChart" that creates the chart.
 //
 define(["d3", "c3", "wc/ajax/ajax"], function (d3, c3, ajax) {
@@ -28,34 +18,33 @@ define(["d3", "c3", "wc/ajax/ajax"], function (d3, c3, ajax) {
 		    label: 'Fruit eaten'
 		},
 		y: {
-		    label: 'How much eaten',
+		    label: 'How much eaten'
 		}
 	    },
 	    oninit: requestData
-
 	});
 
 	function requestData()
 	{
 	    var callback = function (data) {
-		var series, cols, item, c3Struct;
-		cols = [];
-		// Change the JSON data into the C3 column data structure
-		var series = JSON.parse(data);
-		series.forEach(function (next) {
-		    // Get the array of data
-		    item = next.data;
-		    // Put the name used for the data at the start
-		    item.unshift(next.name);
-		    // Add to columns
-		    cols.push(item);
-		});
-		// Create the c3 Columns Structure
-		c3Struct = {
-		    columns: cols
-		};
-		// Load data into chart
-		chart.load(c3Struct);
+			var series, cols, item, c3Struct;
+			cols = [];
+			// Change the JSON data into the C3 column data structure
+			var series = JSON.parse(data);
+			series.forEach(function (next) {
+				// Get the array of data
+				item = next.data;
+				// Put the name used for the data at the start
+				item.unshift(next.name);
+				// Add to columns
+				cols.push(item);
+			});
+			// Create the c3 Columns Structure
+			c3Struct = {
+				columns: cols
+			};
+			// Load data into chart
+			chart.load(c3Struct);
 	    };
 	    ajax.simpleRequest({url: url, callback: callback});
 	}
@@ -63,7 +52,7 @@ define(["d3", "c3", "wc/ajax/ajax"], function (d3, c3, ajax) {
     }
 
     return function (url, containerId, type) {
-	new FruitChart(url, containerId, type);
+		new FruitChart(url, containerId, type);
     };
 
 });
